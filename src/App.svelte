d52d33d55ee7e13dfc72b5229fe71c8eac81fded
Mohammed Batis <your_email@example.com>
2026-03-12 15:37:43 +0800

    Build overview layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,11 @@
 <script>
-  import Explainer from './Explainer.svelte';
-  import Header from './Header.svelte';
+  import Explainer from "./Explainer.svelte";
+  import Header from "./Header.svelte";
 </script>
-
-<style>
-</style>
 
 <div id="app-page">
   <recommender-overlay
-    my-brand="CNN Explainer"
+    my-brand="CNN Visualizer"
     brands-to-ignore="CNN 101"
     recommendation-count="10"
     similar-candidate-count="15"
@@ -19,3 +16,6 @@
   <Header />
   <Explainer />
 </div>
+
+<style>
+</style>
